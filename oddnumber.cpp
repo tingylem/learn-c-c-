2c949ab596c43bb7e6db4b9acb967dd1ba603ac2
tingylem <tingylem@gmail.com>
2019-04-24 09:41:51 -0400

    odd number

--- oddnumber.cpp
+++ oddnumber.cpp
@@ -1,1 +1,20 @@
-/* using bitwise & 
+/* using bitwise & to find odd number
+ * all binary odd number ending 1
+ * */
+
+#include<iostream>
+
+using namespace std;
+
+int main ( int argc, char *argv[])
+{
+	int num;
+	cout<<"enter number"<<endl;
+	cin>>num;
+
+	if(num & 1)
+		cout<<"odd number"<<endl;
+	else
+		cout<<"even number"<<endl;
+	return 0;
+}
